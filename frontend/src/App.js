edcaa965d6ddeb124db6bda13e64fcdc864bac59
Yashwanth137 <Yashwanth137@users.noreply.github.com>
2025-06-21 23:52:58 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import { useEffect, useState } from "react";
 function App() {
   const [msg, setMsg] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then(res => res.json())
-      .then(data => setMsg(data.message));
-  }, []);
+ useEffect(() => {
+  fetch("http://localhost:8000/")
+    .then(res => res.json())
+    .then(data => {
+      console.log("Fetched data:", data);
+      setMsg(JSON.stringify(data.sample_user, null, 2));
+    })
+    .catch(err => console.error("Fetch error:", err));
+}, []);
 
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>AI Recruitment Tool</h1>
+      <h1>LinkedIn Talent Finder</h1>
       <p>Backend says: {msg}</p>
     </div>
   );
